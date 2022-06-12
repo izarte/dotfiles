f49cc702128aeb8ff1479b512fb53acd0facd18f
inigo183 <inigozarate01@gmail.com>
2022-06-12 16:59:50 +0200

    modify {} variables install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Copy repository configuration folders into user home directory
 tools=$(ls config)
 
-for tool in $tools; do
-  cp -rf "config/$tool" "$HOME/.config"
+for tool in ${tools}; do
+  cp -rf "config/$i{tool}" "${HOME}/.config"
 done
 
